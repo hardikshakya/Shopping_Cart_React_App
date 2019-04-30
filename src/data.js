12d80b7b08a331bf0b89b8d92706db295c46cf0d
Hardik Shakya <hardikshakya1997@gmail.com>
2019-04-30 12:20:04 +0530

    Added data.js and changes in App.css

---
+++ src/data.js
@@ -0,0 +1,72 @@
+export default [
+    {
+        id: 101,
+        title: 'Angular Router',
+        author: 'Victor Savkin',
+        description: `This book is a comprehensive guide to the Angular router written by its designer, who is a co-founder Nrwl - Angular Consulting for enterprise customers, from core contributors.`,
+        price: 25,
+        category: 'Angular',
+        inStock: 5,
+        image: 'https://s3.amazonaws.com/titlepages.leanpub.com/router/hero?1482615300.jpg'
+    },
+    {
+        id: 201,
+        title: 'Programming React Native',
+        author: 'Dotan Nahum',
+        description: `In this book, we aim to build cross-platform mobile apps, for Android and iOS, using React Native. We'll learn what it means to build a robust application architecture that will stay with you regardless of change in the tooling or ecosystem churn`,
+        price: 11.99,
+        category: 'React Native',
+        inStock: 4,
+        image: 'https://s3.amazonaws.com/titlepages.leanpub.com/programming-react-native/hero?1467374443.jpg'
+    },
+    {
+        id: 301,
+        title: 'SurviveJS - React',
+        author: 'Juho Vepsäläinen',
+        description: `SurviveJS - React shows you how to build a simple Kanban using React. The idea is that if you can build a simple application, you can probably build something more complex after that. The first application is always the hardest and that's where this book comes in.`,
+        price: 15.99,
+        category: 'React',
+        inStock: 2,
+        image: 'https://s3.amazonaws.com/titlepages.leanpub.com/survivejs-react/hero?1464783722.jpg'
+    },
+    {
+        id: 401,
+        title: 'The Majesty of Vue.js 2',
+        author: 'Alex Kyriakidis et al',
+        description: `This book will guide you through the path of the rapidly spreading Javascript Framework Vue.js! By the end of this book, you will be able to create fast front end applications and increase the performance of your existing projects with Vue.js 2 integration.`,
+        price: 31.90,
+        category: 'Vue',
+        inStock: 6,
+        image: 'https://s3.amazonaws.com/titlepages.leanpub.com/vuejs2/hero?1490059830.jpg'
+    },
+    {
+        id: 501,
+        title: 'Practical Vue.js',
+        author: 'Daniel Schmitz et al',
+        description: `Master this simple yet powerful framework to create reactive web interfaces with practical examples.`,
+        price: 25.99,
+        category: 'Vue',
+        inStock: 1,
+        image: 'https://s3.amazonaws.com/titlepages.leanpub.com/vue/hero?1468433813.jpg'
+    },
+    {
+        id: 601,
+        title: `Build APIs You Won't Hate`,
+        author: 'Phil Sturgeon',
+        description: `Tasked with building an API for your company but don't have a clue where to start? Taken over an existing API and hate it? Built your own API and still hate it? This book is for you.`,
+        price: 29.99,
+        category: 'Backend',
+        inStock: 10,
+        image: 'https://s3.amazonaws.com/titlepages.leanpub.com/build-apis-you-wont-hate/hero?1465835694.jpg'
+    },
+    {
+        id: 801,
+        title: 'Angular 2',
+        author: 'Sebastian Eschweiler',
+        description: `A Practical Introduction to the new Web Development Platform Angular 2 (Angular.js, Angular.js 2, AngularJS, AngularJS 2, ng2)`,
+        price: 24.80,
+        category: 'Angular',
+        inStock: 0,
+        image: 'https://s3.amazonaws.com/titlepages.leanpub.com/angular2-book/hero?1480420187.jpg'
+    },
+]

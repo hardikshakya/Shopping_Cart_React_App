72420a4671e48cb4353d529d393866223609a655
Hardik Shakya <hardikshakya1997@gmail.com>
2019-04-30 11:34:22 +0530

    Crated first component BookItem.js

---
+++ src/components/BookItem/BookItem.js
@@ -0,0 +1,44 @@
+import React, { Component} from 'react';
+
+import './BookIten.css';
+
+class BookIten extends Component{
+    render()
+    {
+        return(
+            <div className="BookIten">
+                <div className="BookIten__img"
+                style={{
+                    backgroundImage: `url($\{book.image\})`,
+                    width: '800px',
+                    height: 300,
+                    backgroundSize: 'contain',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition: 'top left'
+                }}
+                >
+                    <img src={book.image} alt="Book Item"></img>
+                </div>
+                <div className="BookItem__details">
+                    <div className="BookItem__cat">
+                        {book.category}
+                    </div>
+                    <h4>{book.title}</h4>
+                    <h5>{book.author}</h5>
+                    <div className="BookItem__price-cart">
+                        <p>${book.price}</p>
+                        <button onClick={this.props.handleClick.bind(this, book)}>
+                            <span className="fa fa-cart-plus"></span> 
+                            Buy
+                        </button>
+                    </div>
+                    <div className="BookItem__stock" style={{color: book.inStock >= 5 ? '#417505' : '#CE0814'}}>
+                        {book.inStock} In Stock
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default BookIten;
